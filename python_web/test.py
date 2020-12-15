a11d0416feb381a8fd771620aec3e7afe932794f
unknown <imbamonko@126.com>
2020-12-15 19:18:05 +0800

    修改了issue

--- python_web/test.py
+++ python_web/test.py
@@ -1,6 +1,11 @@
 import sqlite3
+from werkzeug.security import generate_password_hash
 conn = sqlite3.connect('../../RUCCA.db')
 cursor = conn.cursor()
+cursor.execute("UPDATE person_info SET id = 2 WHERE username = 'Suzumiya_Konata'")
+cursor.execute("INSERT INTO person_info(username,password_hash) VALUES('linrun',?)", (generate_password_hash('linrun'),))
 cursor.execute("SELECT * FROM person_info")
 v = cursor.fetchall()
+conn.commit()
+conn.close()
 print(v)
