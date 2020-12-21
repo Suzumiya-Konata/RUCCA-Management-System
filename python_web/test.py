ba835a4b4d1fdbc9f6893af0ece546cd6646ea20
run lin <576628923@qq.com>
2020-12-21 13:32:01 +0800

    完成活动相关功能

--- python_web/test.py
+++ python_web/test.py
@@ -2,9 +2,7 @@ import sqlite3
 from werkzeug.security import generate_password_hash
 conn = sqlite3.connect('../../RUCCA.db')
 cursor = conn.cursor()
-cursor.execute("UPDATE person_info SET id = 2 WHERE username = 'Suzumiya_Konata'")
-cursor.execute("INSERT INTO person_info(username,password_hash) VALUES('linrun',?)", (generate_password_hash('linrun'),))
-cursor.execute("SELECT * FROM person_info")
+cursor.execute("SELECT * FROM person_info WHERE id = 14")
 v = cursor.fetchall()
 conn.commit()
 conn.close()
